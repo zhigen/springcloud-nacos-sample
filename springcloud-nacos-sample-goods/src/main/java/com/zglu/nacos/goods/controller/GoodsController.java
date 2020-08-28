4ff27d11007782cad1356b12355a3f787a8da96c
zhigen <385861131@qq.com>
2020-08-28 15:38:42 +0800

    远程事务正常回滚

--- springcloud-nacos-sample-goods/src/main/java/com/zglu/nacos/goods/controller/GoodsController.java
+++ springcloud-nacos-sample-goods/src/main/java/com/zglu/nacos/goods/controller/GoodsController.java
@@ -2,6 +2,7 @@ package com.zglu.nacos.goods.controller;
 
 import com.zglu.nacos.common.api.GoodsApi;
 import com.zglu.nacos.common.feign.UserFeign;
+import io.seata.spring.annotation.GlobalTransactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.annotation.Transactional;
@@ -31,6 +32,7 @@ public class GoodsController implements GoodsApi {
 
     @Override
     @Transactional(rollbackFor = Exception.class)
+    @GlobalTransactional
     public int goodsAndUserAdd() {
         String sql = "insert into zglu1.goods (name, created_date, created_by, last_modified_date, last_modified_by, deleted) values (?, ?, ?, ?, ?, ?)";
         jdbcTemplate.update(sql, "seata", new Date(), 0, new Date(), 0, 0);
